a7c93e68840049619e09dde7da56457b6773c900
Klelik <bam.michaelis@gmail.com>
2024-04-27 15:40:55 -0600

    Prep one finished

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,16 @@ using System;
 
 class Program
 {
+    static string Uppercase(string pizza)
+    {
+        return pizza.Substring(0,1).ToUpper() + pizza.Substring(1).ToLower();
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname = Uppercase(Console.ReadLine());
+        Console.Write("What is your last name? ");
+        string lname = Uppercase(Console.ReadLine());
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
 }
