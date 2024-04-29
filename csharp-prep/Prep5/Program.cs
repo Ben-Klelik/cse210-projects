f094d89df0a7c780058a7e658efa396dc5118b4e
Ben-Klelik <bam.michaelis@gmail.com>
2024-04-29 13:43:34 -0600

    prep 5 completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine($"Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write($"Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static string PromptUserNumber()
+    {
+        Console.Write($"Enter your favorite number: ");
+        return Console.ReadLine();
+    }
+
+    static double SquareNumber(double num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, double squared)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        var userName = PromptUserName();
+        var userNum = double.Parse(PromptUserNumber());
+        DisplayResult(userName, SquareNumber(userNum));
     }
 }
