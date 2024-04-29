8b6906fdd44d5d6e93279b2ae9733a9fd929812d
Ben-Klelik <bam.michaelis@gmail.com>
2024-04-29 13:05:30 -0600

    Prep 4 and stretch complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int input;
+        var numberList = new List<int>();
+        Console.WriteLine("Give a list of numbers (enter 0 to end the list):");
+        while (true)
+        {
+            Console.Write("n: ");
+            input = int.Parse(Console.ReadLine());
+            if (input == 0) { break; }
+            numberList.Add(input);
+        }
+
+        double sum = numberList.Sum();
+        double min = numberList.Where(n => n >= 0).Min();
+        double max = numberList.Max();
+        double avg = numberList.Average();
+        Console.WriteLine($"Sum : {sum}");
+        Console.WriteLine($"Average : {avg}");
+        Console.WriteLine($"Largest num : {max}");
+        Console.WriteLine($"Smallest positive : {min}");
+        Console.WriteLine($"Ordered list : ");
+        numberList.Sort();
+        foreach (int num in numberList)
+        {
+            Console.WriteLine($": {num}");
+        }
     }
 }
