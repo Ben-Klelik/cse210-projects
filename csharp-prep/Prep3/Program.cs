c8ae386e7705fa274aa290ce976d33cd83e99720
Ben-Klelik <bam.michaelis@gmail.com>
2024-04-29 12:07:23 -0600

    Prep 3 and stretch completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        var rng = new Random();
+        do
+        {
+            int input;
+            int guesses = 0;
+            int magicNumber = rng.Next(1, 101);
+            Console.WriteLine("Guess a number between 1 and 100");
+            do
+            {
+                Console.WriteLine("Guess: ");
+                input = int.Parse(Console.ReadLine());
+                if (input < magicNumber)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                else if (input > magicNumber)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+                guesses++;
+            } while (input != magicNumber);
+            Console.WriteLine($"Correct! You got it in {guesses} guesses.");
+            Console.WriteLine("Do you want to play again (Y/N)");
+        } while (Console.ReadLine().ToLower() == "y");
     }
 }
