ef7626d43d38ea275973de962d10582b399c4a76
Ben-Klelik <bam.michaelis@gmail.com>
2024-06-07 14:18:12 -0600

    Completed learning activity four

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new("Himdo Nominim", "Architechture");
+        Console.WriteLine(assignment.GetSummary());
+        MathAssignment mathAssignment = new("Himdo Nominimim", "Math", "4.5", "1-40");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        WritingAssignment writingAssignment = new("Himdi Monimin", "English 250", "The great gastby by Bigmin Manly");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
